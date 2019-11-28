23da6e9abf55eb098d99c90cbd5242535057049a
Inês Oliveira <inesfpoliveira@gmail.com>
2019-11-28 21:00:37 +0000

    come on pls

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,9 +2,14 @@ import React from "react";
 
 function Logo () {
     return (
-        <div className="logo">
-            Hackathon 2 - Christmas
-        </div>
+        <>
+            <div className="logo">
+                Hackathon 2 -
+            </div>
+            <div className="logo">
+                Christmas
+            </div>
+        </>
     );
 }
 
